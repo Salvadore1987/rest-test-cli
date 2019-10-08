24f5ca32d95ffc7742a08a61de239e276b00e06c
eldar <123456>
2019-10-08 11:18:05 +0500

    Ref

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const init = (args: string[]): void => {
         const method = args[2].toUpperCase();
         const count = parseInt(args[3]);
         const url = args[4];
-        testUrl(url);
+        _testUrl(url);
         if (method == 'GET')
             doGet(url, count);
         if (method == 'POST')
@@ -28,7 +28,7 @@ const doGet = async (url: string, count: number): Promise<void> => {
         let result: IResult;
         try {
             let begin = Date.now();
-            const res = await get(url);
+            const res = await _get(url);
             let end = Date.now();
             result = {error: false, success: true, answerTime: end - begin}
             infoLog(res.data);
@@ -40,12 +40,12 @@ const doGet = async (url: string, count: number): Promise<void> => {
     }
     const end = Date.now();
     const totalReqTime = Math.round(((end - begin) * 100) / 100);
-    printReport(count, totalReqTime);
+    _printReport(count, totalReqTime);
     
 }
 
-const printReport = (count: number, totalReqTime: number): void => {
-    const res = calculate(count, totalReqTime);
+const _printReport = (count: number, totalReqTime: number): void => {
+    const res = _calculate(count, totalReqTime);
     const total = chalk.green('Total requests: ' + res.total);
     const success = chalk.yellow('Success requests: ' + res.success + '%');
     const rejected = chalk.red('Rejected requests: ' + res.rejected + '%');
@@ -69,7 +69,7 @@ const doPost = async (url: string, count: number, data?: any): Promise<void> => 
         let result: IResult;
         try {
             let begin = Date.now();
-            const res = await post(url, data);
+            const res = await _post(url, data);
             let end = Date.now();
             result = {error: false, success: true, answerTime: end - begin}
             infoLog(res.data);
@@ -81,58 +81,58 @@ const doPost = async (url: string, count: number, data?: any): Promise<void> => 
     }
     const end = Date.now();
     const totalReqTime = Math.round(((end - begin) * 100) / 100);
-    printReport(count, totalReqTime);
+    _printReport(count, totalReqTime);
 }
 
-const testUrl = (url: string): void => {
+const _testUrl = (url: string): void => {
     if (!url.match(regex))
         throw new Error('Url is not valid');
 }
 
-const get = async (url: string): Promise<any> => {
+const _get = async (url: string): Promise<any> => {
     const res = await axios.get(url);
     return res;
 }
 
-const post = async (url:string, data: any): Promise<any> => {
+const _post = async (url:string, data: any): Promise<any> => {
     const res = await axios.post(url, data);
     return res;
 }
 
-const calculate = (count: number, totalReqTime: number): IReport => {
+const _calculate = (count: number, totalReqTime: number): IReport => {
     const report: IReport = {
         total: count, 
-        rejected: allRejected(count), 
-        success: allSuccess(count), 
-        avverageTime: average(), 
+        rejected: _allRejected(count), 
+        success: _allSuccess(count), 
+        avverageTime: _average(), 
         totalRequestTime: totalReqTime,
-        minRequestTime: min(),
-        maxRequestTime: max()
+        minRequestTime: _min(),
+        maxRequestTime: _max()
     };
     return report;
 }
 
-const min = (): number => {
+const _min = (): number => {
     const res = results.filter(i => i.success == true).map(i => i.answerTime).reduce((min, p) => p < min ? p : min);
     return Math.round((res * 100) / 100);
 }
 
-const max = (): number => {
+const _max = (): number => {
     const res = results.filter(i => i.success == true).map(i => i.answerTime).reduce((max, p) => p > max ? p : max);
     return Math.round((res * 100) / 100);
 }
 
-const average = (): number => {
+const _average = (): number => {
     const res = results.filter(i => i.success == true).map(i => i.answerTime).reduce((val, idx) => val += idx);
     return res / results.filter(i => i.success).length;
 }
 
-const allRejected = (count: number): number => {
+const _allRejected = (count: number): number => {
     const res = results.filter(i => i.error == true).map(i => i).length;
     return res * 100 / count;
 }
 
-const allSuccess = (count: number): number => {
+const _allSuccess = (count: number): number => {
     const res = results.filter(i => i.success == true).map(i => i).length;
     return res * 100 / count;
 }
